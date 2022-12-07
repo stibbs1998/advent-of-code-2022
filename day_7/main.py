a0a3ec2eee63b4381241a0395044fd8edcaef620
Spencer Tibbitts <stibbs1998@gmail.com>
2022-12-07 13:15:00 -0500

    part 2

--- day_7/main.py
+++ day_7/main.py
@@ -1,4 +1,27 @@
 INPUT = """
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
 """
 
 
@@ -13,18 +36,17 @@ def sum_dict(dictionary):
     return counter
 
 
-def walk_path(path_sizes, obj):
+def walk_path(directory_space, obj):
     sum_for_path = sum_dict(obj)
-    path_sizes.append(sum_for_path)
+    directory_space.append(sum_for_path)
     for k, v in obj.items():
         if isinstance(v, dict):
-            path_sizes.append(walk_path(path_sizes, v))
+            directory_space.append(walk_path(directory_space, v))
 
-    return path_sizes
+    return directory_space
 
 
-def solve(my_input):
-
+def gather_system_files(my_input):
     my_filepath = {}
     pwd = my_filepath
     prev_wd = []
@@ -41,21 +63,48 @@ def solve(my_input):
                 prev_wd.append(pwd)  # Add previous folder to the stack
                 pwd[dir_name] = {}  # Initialize folder
                 pwd = pwd[dir_name]  # Make folder current directory
-        if line[0] in "1234567890":
+        if line[0] in "1234567890":  # A file has appeared
             size, file_name = line.split(" ")
             pwd[file_name] = int(size)
 
-    messy_file_sizes = walk_path([], my_filepath)
-    total_disk_space = messy_file_sizes[0]  # outermost path sum
-    final_counter = 0
-    for item in messy_file_sizes:
-        if isinstance(item, int):
-            if item <= 100_000:
-                final_counter += item
+    # my_filepath = gather_system_files(my_input)
+    directory_space = walk_path([], my_filepath)
+    return directory_space
 
+
+def solve_part_1(my_input):
+
+    final_counter = 0
+    for item in gather_system_files(my_input):
+        if isinstance(item, int) and item <= 100_000:
+            final_counter += item
     return final_counter
+
+
+def solve_part_2(my_input):
+    # Part 2
+    MAX_DISK_SPACE = 70_000_000
+    DESIRED_SPACE = 30_000_000
+
+    directory_space = gather_system_files(my_input)
+    CURRENT_SPACE = (
+        MAX_DISK_SPACE - directory_space[0]
+    )  # Top level directory, which is total disk space
+
+    cheapest_to_delete = MAX_DISK_SPACE
+
+    for item in directory_space:
+        if (
+            isinstance(item, int)
+            and CURRENT_SPACE + item > DESIRED_SPACE
+            and item < cheapest_to_delete
+        ):  # if we reclaim that space, and it pushes us over DESIRED_SPACE
+            cheapest_to_delete = item
+
+    return cheapest_to_delete
 
 
 if __name__ == "__main__":
 
-    print(solve(INPUT))
+    print(solve_part_1(INPUT))
+    print(solve_part_2(INPUT))
